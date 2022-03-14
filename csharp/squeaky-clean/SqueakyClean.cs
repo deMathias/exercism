1691782ecc0c7e13a92f7c8c1f62e41327255b23
deMathias <47175867+deMathias@users.noreply.github.com>
2022-03-14 16:17:47 +0100

    Solved Squeaky Clean
    Solved Squeaky Clean

---
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class Identifier
+{
+    public static string Clean(string identifier)
+    {
+        string strReturnString = "";
+        Boolean boolShouldIMakeUpper = false;
+        foreach (char c in identifier)
+        {
+            if (c == ' ')
+            {
+                strReturnString += "_";
+            }
+            else if (char.IsControl(c))
+            {
+                strReturnString += "CTRL";
+            }
+            else if (c == '-')
+            {
+                boolShouldIMakeUpper = true;
+            }
+            else if (!char.IsLetter(c))
+            {
+
+            }
+            else if (boolShouldIMakeUpper)
+            {
+                strReturnString += Char.ToUpper(c);
+                boolShouldIMakeUpper = false;
+            }
+            else if (Regex.IsMatch(c.ToString(),"[α-ω]",RegexOptions.None))
+            {
+
+            }
+            else
+            {
+                strReturnString += c;
+            }
+        }
+        return strReturnString;
+    }
+}
