e2e89cdd158a6babb4cbcc5351f545dfd9d1c370
deMathias <47175867+deMathias@users.noreply.github.com>
2022-03-14 15:16:25 +0100

    Fixed bool evaluation
    fixed bool eval
    
    Co-Authored-By: NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>

--- csharp/annalyns-infiltration/AnnalynsInfiltration.cs
+++ csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -9,6 +9,7 @@ static class QuestLogic
 
     public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
     {
+        /*
         if (!knightIsAwake && !archerIsAwake && !prisonerIsAwake)
         {
             return false;
@@ -17,16 +18,22 @@ static class QuestLogic
         {
             return true;
         }
+        */
+        //Spy: the group can be spied upon if at least one of them is awake. Otherwise, spying is a waste of time. so || instead of && as in the previous solution because we approach the problem different.
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
     }
 
     public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
     {
+        /*
         if(!archerIsAwake && prisonerIsAwake){
             return true;
         }
         else{
             return false;
         }
+        */
+        return !archerIsAwake && prisonerIsAwake;
     }
 
     public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
